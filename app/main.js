961cdab42431884c96b178b8ffbd92406e971224
lambda <lambda@arigato.tech>
2017-04-06 16:20:55 +0300

    sum refactor

--- app/main.js
+++ app/main.js
@@ -1,66 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   TouchableOpacity
-} from 'react-native';
+} from 'react-native'
 
-import styles from './styles';
+import styles from './styles'
 
-import * as Animatable from 'react-native-animatable';
-import SAMPLE_GROUPS from './samples';
+import * as Animatable from 'react-native-animatable'
+import SAMPLE_GROUPS, { getSamplesGroup, getSample } from './samples'
 
-import { createIconSetFromFontello } from 'react-native-vector-icons';
+import { createIconSetFromFontello } from 'react-native-vector-icons'
 
-import fontelloConfig from '../ios/fontello/config.json';
+import fontelloConfig from '../ios/fontello/config.json'
 
-const Icon = createIconSetFromFontello(fontelloConfig);
+const Icon = createIconSetFromFontello(fontelloConfig)
 
-const getRandomSample = group => group[Math.floor(Math.random() * group.length)];
+const getRandomSample = group => group[Math.floor(Math.random() * group.length)]
+const stopSample = sample => sample.stop().release()
+
+function playSample (sample, groupIndex) {
+
+  const onStop = (success) => {
+
+    // todo clone sample before play
+
+    stopSample(sample)
+
+    if (success) {
+
+    }
+
+    const currentGroup = getSamplesGroup(groupIndex)
+    const nextSample = getSample(getRandomSample(currentGroup))
+
+    // stopSample(nextSample)
+    playSample(nextSample, groupIndex)
+
+    console.log(`group: ${groupIndex}, sample changed from ${sample._key} to ${nextSample._key}`)
+  }
+
+  sample.play(onStop)
+}
+
+const sampleFiles = SAMPLE_GROUPS.map(getRandomSample)
+const startSamples = sampleFiles.map(getSample)
+
 
 export default class MainView extends Component {
 
   state = {
     playing: false,
     volume: 0.5,
-    currentSamples: SAMPLE_GROUPS.map(getRandomSample)
-  };
+    currentSamples: startSamples
+  }
 
   playSound = () => {
     // stop all samples
-    const { currentSamples } = this.state;
-    currentSamples.map(sample => sample.stop().release());
+    const {currentSamples, playing} = this.state
+    currentSamples.map(stopSample)
 
     // make sum beatz
-    const newSamples = SAMPLE_GROUPS.map(getRandomSample);
-    newSamples.map(sample => sample.play());
-
+    const newSamples = SAMPLE_GROUPS.map(getRandomSample)
     // show whats playing
-    console.log(`now playing: ${newSamples.map(sample => sample._key)}`);
+    console.log(`now playing: ${newSamples.map(sample => sample._key)}`)
 
     this.setState({
       playing: true,
       currentSamples: newSamples
-    });
+    })
+
+    newSamples.map((sample, groupIndex) => playSample(getSample(sample), groupIndex))
   }
 
   stopSound = () => {
-    const { currentSamples } = this.state;
+    const {currentSamples} = this.state
     // stop all sounds
-    currentSamples.map(sample => sample.stop().release());
-    console.log('stop looping');
+    currentSamples.map(stopSample)
+    console.log('stop looping')
 
-    this.setState({ playing: false });
+    this.setState({playing: false})
   }
 
   toggleSound = () => {
-    const { playing } = this.state;
+    const {playing} = this.state
     if (playing) {
-      this.stopSound();
+      this.stopSound()
     } else {
-      this.playSound();
+      this.playSound()
     }
   }
 
-  render() {
+  render () {
     // todo move btn to
 
     return (
@@ -82,7 +111,7 @@ export default class MainView extends Component {
           />
         </TouchableOpacity>
       </Animatable.View>
-    );
+    )
   }
 
 }
