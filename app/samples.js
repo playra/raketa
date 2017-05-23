1fd37eb47da1b0c2c3f5e18a3219c2b600096250
Dmitriy Vasilev <xyz@MacBook-Pro-Dmitriy.local>
2017-05-23 16:33:27 +0300

    new sound pack

--- app/samples.js
+++ app/samples.js
@@ -6,140 +6,94 @@ import {
 import Sample from './core/Sample'
 
 // test loop
-const TABLA = 'TABLA.wav'
-const DRUMS1 = 'DRUMS1.m4a'
-const HATS1 = 'HATS1.m4a'
+// const TABLA = 'TABLA.wav'
+// const DRUMS1 = 'DRUMS1.m4a'
+// const HATS1 = 'HATS1.m4a'
 export const SILENCE = 'silence.aif'
 
 // G1
-const DR_BASS = 'DR_BASS.m4a'
-const DR_IN_OUT = 'DR_IN_OUT.m4a'
-const DR_KICK_BASS = 'DR_KICK_BASS.m4a'
-const DR_MAIN = 'DR_MAIN.m4a'
+const S5_AFTERBEATISILENCE_16BEATS = 'S5_AFTERBEATISILENCE_16BEATS.m4a'
+const S5_BDBASS_16BEATS = 'S5_BDBASS_16BEATS.m4a'
+const S5_FILLBEAT_32BEATS = 'S5_FILLBEAT_32BEATS.m4a'
+const S5_MAINBEAT_16BEATS = 'S5_MAINBEAT_16BEATS.m4a'
 
 const GROUP_1 = [
-  DR_BASS,
-  DR_IN_OUT,
-  DR_KICK_BASS,
-  DR_MAIN
+  S5_AFTERBEATISILENCE_16BEATS,
+  S5_BDBASS_16BEATS,
+  S5_FILLBEAT_32BEATS,
+  S5_MAINBEAT_16BEATS
 ]
 
 // G2
-const BASS_1 = 'BASS_1.m4a'
-const BASS_2 = 'BASS_2.m4a'
-const BASS_3 = 'BASS_3.m4a'
-const BASS_4 = 'BASS_4.m4a'
+const S5_BASS1_16BEATS = 'S5_BASS1_16BEATS.m4a'
+const S5_BASS2_32BEATS = 'S5_BASS2_32BEATS.m4a'
+const S5_BASS3_16BEATS = 'S5_BASS3_16BEATS.m4a'
 
 const GROUP_2 = [
-  BASS_1,
-  BASS_2,
-  BASS_3,
-  BASS_4
+  S5_BASS1_16BEATS,
+  S5_BASS2_32BEATS,
+  S5_BASS3_16BEATS
 ]
 
 // G3
-const FX_1 = 'FX_1.m4a'
-const FX_2 = 'FX_2.m4a'
-const FX_3 = 'FX_3.m4a'
-const FX_4 = 'FX_4.m4a'
-const FX_5 = 'FX_5.m4a'
-const FX_6 = 'FX_6.m4a'
+const S5_HATS1_16BEATS = 'S5_HATS1_16BEATS.m4a'
+const S5_HATS2_16BEATS = 'S5_HATS2_16BEATS.m4a'
 
 const GROUP_3 = [
-  FX_1,
-  FX_2,
-  FX_3,
-  FX_4,
-  FX_5,
-  FX_6
+  S5_HATS1_16BEATS,
+  S5_HATS2_16BEATS
 ]
 
 // G4
-const HATS_1 = 'HATS_1.m4a'
-const HATS_2 = 'HATS_2.m4a'
-const HATS_3 = 'HATS_3.m4a'
-const HATS_4 = 'HATS_4.m4a'
+const S5_CHORDS1_32BEATS = 'S5_CHORDS1_32BEATS.m4a'
+const S5_CHORDS3_32BEATS = 'S5_CHORDS3_32BEATS.m4a'
+const S5_CHORDS_32BEATS = 'S5_CHORDS_32BEATS.m4a'
 
 const GROUP_4 = [
-  HATS_1,
-  HATS_2,
-  HATS_3,
-  HATS_4
+  S5_CHORDS1_32BEATS,
+  S5_CHORDS3_32BEATS,
+  S5_CHORDS_32BEATS
 ]
 
 // G5
-const PAD_1 = 'PAD_1.m4a'
-const PAD_2 = 'PAD_2.m4a'
-const PAD_3 = 'PAD_3.m4a'
-const PAD_4 = 'PAD_4.m4a'
-const PAD_5 = 'PAD_5.m4a'
-const PAD_6 = 'PAD_6.m4a'
+const S5_SYNTH1_16BEATS = 'S5_SYNTH1_16BEATS.m4a'
+const S5_SYNTH2_32BEATS = 'S5_SYNTH2_32BEATS.m4a'
 
 const GROUP_5 = [
-  PAD_1,
-  PAD_2,
-  PAD_3,
-  PAD_4,
-  PAD_5,
-  PAD_6
+  S5_SYNTH1_16BEATS,
+  S5_SYNTH2_32BEATS
 ]
 
 // G6
-const PERC_1 = 'PERC_1.m4a'
-const PERC_2 = 'PERC_2.m4a'
-const PERC_3 = 'PERC_3.m4a'
-const PERC_4 = 'PERC_4.m4a'
-const PERC_5 = 'PERC_5.m4a'
+const S5_PERC1_16BEATS = 'S5_PERC1_16BEATS.m4a'
 
 const GROUP_6 = [
-  PERC_1,
-  PERC_2,
-  PERC_3,
-  PERC_4,
-  PERC_5
+  S5_PERC1_16BEATS
 ]
 
 // G7
-const SYNTH_1 = 'SYNTH_1.m4a'
-const SYNTH_2 = 'SYNTH_2.m4a'
-const SYNTH_3 = 'SYNTH_3.m4a'
-const SYNTH_4 = 'SYNTH_4.m4a'
-const SYNTH_5 = 'SYNTH_5.m4a'
+const S5_FX_32BEATS = 'S5_FX_32BEATS.m4a'
 
 const GROUP_7 = [
-  SYNTH_1,
-  SYNTH_2,
-  SYNTH_3,
-  SYNTH_4,
-  SYNTH_5
+  S5_FX_32BEATS
 ]
 
 // G8
-const VOX_1 = 'VOX_1.m4a'
-const VOX_2 = 'VOX_2.m4a'
-const VOX_3 = 'VOX_3.m4a'
-const VOX_4 = 'VOX_4.m4a'
-const VOX_5 = 'VOX_5.m4a'
-const VOX_6 = 'VOX_6.m4a'
-const VOX_7 = 'VOX_7.m4a'
+const S5_VOX1_32BEATS = 'S5_VOX1_32BEATS.m4a'
+const S5_VOX2_16BEATS = 'S5_VOX2_16BEATS.m4a'
 
 const GROUP_8 = [
-  VOX_1,
-  VOX_2,
-  VOX_3,
-  VOX_4,
-  VOX_5,
-  VOX_6,
-  VOX_7
+  S5_VOX1_32BEATS,
+  S5_VOX2_16BEATS
 ]
 
-const SILENCE_GROUP = [
-  SILENCE
-]
+// const SILENCE_GROUP = [
+//  SILENCE
+// ]
 
-const METRONOME_GROUP = [
-  METRONOME
-]
+// const METRONOME_GROUP = [
+// METRONOME
+// ]
 
 // todo LOOP GAP PER GROUP
 const GROUPS = [
@@ -148,10 +102,10 @@ const GROUPS = [
   GROUP_3,
   GROUP_4,
   GROUP_5,
- // GROUP_6,
+  GROUP_6,
   GROUP_7,
   // SILENCE_GROUP
-  //METRONOME_GROUP
+  // METRONOME_GROUP
   GROUP_8
 ]
 
--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import MainView from './app/main';
+import { AppRegistry } from 'react-native'
+import MainView from './app/main'
 
-AppRegistry.registerComponent('Raketa', () => MainView);
+AppRegistry.registerComponent('Raketa', () => MainView)
